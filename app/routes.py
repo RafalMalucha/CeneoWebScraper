c7cc475e94ff98395ded3814098d8af24bd0d3ae
RafalMalucha <s220986@student.uek.krakow.pl>
2022-05-20 13:03:02 +0200

    2022-05-20 RM

---
+++ app/routes.py
@@ -0,0 +1,11 @@
+from app import app
+from flask import render_template
+
+@app.route('/')
+@app.route('/index/<name>')
+def index(name='<<Hello, World!>>'):
+    return render_template('index.html', text=name)
+
+@app.route('/cos')
+def cos():
+    return 'Rafał Małucha'
